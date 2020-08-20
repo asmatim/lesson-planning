c2daa05f44d9c8eeabcb27de2b64f629751a0a9f
asmatim <atimzit@gmail.com>
2020-08-20 21:13:25 +0200

    Add date binder for Lesson Controller

--- src/main/java/ma/ac/supmti/pfe/controller/LessonController.java
+++ src/main/java/ma/ac/supmti/pfe/controller/LessonController.java
@@ -3,10 +3,17 @@ package ma.ac.supmti.pfe.controller;
 import ma.ac.supmti.pfe.dto.LessonDto;
 import ma.ac.supmti.pfe.facade.LessonFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/lesson")
@@ -19,6 +26,13 @@ public class LessonController {
     @ResponseBody
     public LessonDto planLesson(LessonDto lessonDto) {
         return lessonFacade.save(lessonDto);
+    }
+
+    @InitBinder
+    public void bindingPreparation(WebDataBinder binder) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        CustomDateEditor orderDateEditor = new CustomDateEditor(dateFormat, true);
+        binder.registerCustomEditor(Date.class, orderDateEditor);
     }
 
 }
